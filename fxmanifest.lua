bbfc4659d44d474a9afd0d1492611575572d244e
Jordan.#2139 <jordansilva213@gmail.com>
2022-10-25 19:31:11 -0400

    1.3.2-sonoran version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -38,7 +38,7 @@ game "gta5"
 name "Wraith ARS 2X"
 description "Police radar and plate reader system for FiveM"
 author "WolfKnight"
-version "1.3.1-sonoran"
+version "1.3.2-sonoran"
 
 -- Include the files
 files {
